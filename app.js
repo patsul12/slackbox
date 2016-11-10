eb2b9c18452ca125bb83ce12a73562b1944ed37c
patsul12 <patsul12@gmail.com>
2016-11-10 13:45:40 -0600

    public response

--- app.js
+++ app.js
@@ -72,7 +72,10 @@ app.post('/store', function(req, res) {
           var track = results[0];
           spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:' + track.id])
             .then(function(data) {
-              return res.send('Track added: *' + track.name + '* by *' + track.artists[0].name + '*');
+              return res.send({
+                response_type: 'in_channel',
+                text: 'Track added: *' + track.name + '* by *' + track.artists[0].name + '*'
+              });
             }, function(err) {
               return res.send(err.message);
             });
